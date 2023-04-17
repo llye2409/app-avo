c6b33326a8f75fbe50773a3f0140c0946f1b8bad
llye2409 <117427707+llye2409@users.noreply.github.com>
2023-04-17 22:46:17 +0700

    Update streamlit_helpers.py

--- lib/streamlit_helpers.py
+++ lib/streamlit_helpers.py
@@ -41,44 +41,75 @@ def create_infomation_app(name_app, version_app, current_time):
     )
 
 def create_introdution_app():
+    st.image('assets/images/avocado.png')
     st.markdown("""
-    Chào mừng đến với ứng dụng dự đoán giá bơ cho toàn nước Mỹ! Ứng dụng được phát triển dựa trên các mô hình machine learning như ***XGB regression, ARIMA, Facebook Prophet*** dự đoán giá bơ cho các vùng khác nhau như
+    ## Business Issue
+    "Hass" Avocado - a company based in Mexico, specializes in producing various types of avocados sold in the US.
+    The company operates in multiple regions of the US with 2 types of avocados, regular and organic,
+    packaged according to various standards (Small/Large/XLarge Bags), and have 3 different PLUs (Product Look Up) (4046, 4225, 4770).
+    However, they do not have a model to predict avocado prices for business expansion.
 
-- California, 
-- San Francisco 
-- Los Angeles.
+    => **Objective**: Build a model to predict the average price of "Hass" avocados in the US => consider expanding production and business
 
-Để sử dụng ứng dụng, bạn chọn các tab tương ứng với các vùng cần dự đoán. Sau đó, lựa chọn các tùy chọn và nhập các thông số đầu vào để dự đoán giá bơ trong khoảng thời gian từ 1 đến 5 năm.
+    ## Specific requirements:
+    - **Task 1**: USA's Avocado Average Price
+    Prediction - Use regression algorithms
+    such as Linear Regression, Random Forest,
+    XGB Regressor...
 
-Tuy nhiên, chúng tôi cũng muốn nhấn mạnh rằng mô hình chỉ đạt được độ chính xác khoảng 90% trên tập test, và chỉ hoạt động tốt với dữ liệu dự đoán khoảng 5 năm trở lại.
+    - **Task 2**: Conventional/Organic Avocado
+    Average Price Prediction for the future in
+    California/NewYork... - Use time series
+    algorithms such as ARIMA, Prophet...
 
-Chúng tôi hy vọng rằng ứng dụng của chúng tôi sẽ giúp bạn dự đoán giá bơ một cách chính xác và hiệu quả. 
-Cảm ơn bạn đã sử dụng ứng dụng của chúng tôi!
+
+    ### USA's Avocado AveragePrice Prediction
+    - Using *RandomForestRegressor* to predict avocado prices.
+    - The regression model achieved an accuracy of *90%* on the test set.
+    - It can be applied in practice to predict the average avocado prices in the US.
+    However, the model needs to be carefully used and evaluated regularly to ensure the accuracy of predictions.
+    Additionally, when applying the model to new data, it's important to check if the new data is similar to the data used for training.
+
+    ### Average Price Prediction in California & Other
+   -  Arima model: California (conventional), Los Angeles (organic), San Francisco (conventional)
+    - Facebook Prophet: California (organic)
+    - Potential regions: Los Angeles, San Francisco. The results show that avocado prices are trending upward in the next 5 years.
+    - In practice, the models have good prediction capabilities for data up to 5 years ago.
+
+    ### Selecting potential regions for organic avocados
+    => **Los Angeles** is a potential region due to its large scale (total sales), increasing price trends over the years, and high growth rate.
+
+    => **San Francisco** is a potential region due to its large scale (total sales), increasing price trends over the years, and high growth rate
+
+    ## User guide:
+    To use the application, you select the corresponding tabs for the regions you want to predict. Then, choose the options and enter the input parameters to predict avocado prices for a period of 1 to 5 years.
+
+    However, we also want to emphasize that the model achieves only about 90% accuracy on the test set and works well for predicting data up to about 5 years in advance.
+
+    We hope that our application will help you predict avocado prices accurately and efficiently. Thank you for using our application!
 
     """)
-    st.image('assets/images/avocado.png')
     
 
 def show_about_data():
     st.markdown("""
-    Dữ liệu được lấy trực tiếp từ máy tính tiền của các nhà bán lẻ dựa trên doanh số bán lẻ thực tế của bơ Hass.
+        - Dữ liệu được lấy trực tiếp từ máy tính tiền của các nhà bán lẻ dựa trên doanh số bán lẻ thực tế của bơ Hass.
+        - Dữ liệu đại diện cho dữ liệu lấy từ máy quét bán lẻ hàng tuần cho lượng bán lẻ (National retail volume- units) và giá bơ từ tháng 4/2015 đến tháng 3/2018.
+        - Giá Trung bình (Average Price) phản ánh giá trên một đơn vị (mỗi quả bơ), ngay cả khi nhiều đơn vị (bơ) được bán trong bao.
+        - Mã tra cứu sản phẩm - Product Lookup codes (PLU’s) trong bảng chỉ dành cho bơ Hass, không dành cho các sản phẩm khác.
 
-    Dữ liệu đại diện cho dữ liệu lấy từ máy quét bán lẻ hàng tuần cho lượng bán lẻ (National retail volume- units) và giá bơ từ tháng 4/2015 đến tháng 3/2018.
-    
-    Giá Trung bình (Average Price) trong bảng phản ánh giá trên một đơn vị (mỗi quả bơ), ngay cả khi nhiều đơn vị (bơ) được bán trong bao.
-    
-    Mã tra cứu sản phẩm - Product Lookup codes (PLU’s) trong bảng chỉ dành cho bơ Hass, không dành cho các sản phẩm khác.
-        Some relevant columns in the dataset:
+        Bao gồm các cột:
 
-    - Date - The date of the observation
-    - AveragePrice - the average price of a single avocado
-    - type - conventional or organic
-    - year - the year
-    - Region - the city or region of the observation
-    - Total Volume - Total number of avocados sold
-    - 4046 - Total number of avocados with PLU 4046 sold
-    - 4225 - Total number of avocados with PLU 4225 sold
-    - 4770 - Total number of avocados with PLU 4770 sold
+        - Date - ngày ghi nhận
+        - AveragePrice – giá trung bình của một quả bơ
+        - Type - conventional / organic – loại: thông thường/ hữu cơ
+        - Region – vùng được bán
+        - Total Volume – tổng số bơ đã bán
+        - 4046 – tổng số bơ có mã PLU 4046 đã bán
+        - 4225 - tổng số bơ có mã PLU 4225 đã bán
+        - 4770 - tổng số bơ có mã PLU 4770 đã bán
+        - Total Bags – tổng số túi đã bán
+        - Small/Large/XLarge Bags – tổng số túi đã bán theo size
         """)
         
 
